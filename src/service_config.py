df5a6e1ade397f87c96829f82c26dae41baa36eb
dogus <dogus@mocha>
2017-09-25 15:05:48 +0300

    host names changed

--- src/service_config.py
+++ src/service_config.py
@@ -11,7 +11,34 @@ BASE_FOLDER = os.path.dirname(SOURCE_FOLDER)
 # Server configs
 service = {
     "host" : "0.0.0.0",
-    "port" : "55555",
-    "ZMQ_URL_CR_CL" : "tcp://192.168.0.94:54321",
-    "ZMQ_URL_FACE" : "tcp://192.168.0.94:54444"
+    "port" : "54443",
+    "ZMQ_URL_CR_CL" : "tcp://localhost:54321",
+    "ZMQ_URL_FACE" : "tcp://localhost:54444"
+}
+
+# Available actions
+actions = {
+    "ACTION_START" : 0,
+    "ACTION_STOP" : 1,
+    "ACTION_CHECK" : 2
+}
+
+# Stream-related configs
+stream = {
+    "TYPE_CAR_CLASSIFICATION" : 0,
+    "TYPE_FACE_DETECTION" : 1,
+    "INTERVAL" : 24,
+    "COPY_COUNT" : 5,
+    "RECONNECT_TIME_OUT" : 5,
+    "RECONNECT_TRY_COUNT" : 5
+}
+
+# Configs related to check stream status
+stream_stat = {
+    "auth-user" : "dogus",
+    "auth-pass" : "ares2728",
+    "url" : "http://localhost:8087/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/live/instances/_definst_",
+    "headers" : {
+        'Accept': 'application/json; charset=utf-8',
+    }
 }
