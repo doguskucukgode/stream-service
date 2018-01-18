b1e03603b79ecfb6fc25049a93b61058aa09ced1
dogus <e1630995@ceng.metu.edu.tr>
2018-01-18 16:27:35 +0300

    Bugfix: Latte configs are updated.

--- src/conf/latte.py
+++ src/conf/latte.py
@@ -4,52 +4,25 @@ from conf.plate_conf import PlateConfig
 from conf.stream_conf import StreamConfig
 
 class LattePlateConfig(PlateConfig):
-
-    detector_server = {
-        "gpu_to_use" : "0"
-    }
-
-    recognizer_server = {
-        "gpu_to_use" : "0"
-    }
-
-    plate_server = {
-        "gpu_to_use" : "0"
-    }
+    PlateConfig.detector_server["gpu_to_use"] = "0"
+    PlateConfig.recognizer_server["gpu_to_use"] = "0"
+    PlateConfig.plate_server["gpu_to_use"] = "0"
 
 
 class LatteStreamConfig(StreamConfig):
-
-    service = {
-        "gpu_to_use" : "0"
-    }
-
-    ipcam_demo = {
-        "in_demo_mode" : False,
-    }
+    StreamConfig.service["gpu_to_use"] = "0"
+    StreamConfig.ipcam_demo["in_demo_mode"] = False
 
 
 class LatteCarConfig(CarConfig):
-
-    cropper = {
-        "gpu_memory_frac" : 0.2,
-        "gpu_to_use" : "0"
-    }
-
-    classifier = {
-        "gpu_memory_frac" : 0.2,
-    }
-
-    crcl = {
-        "classifier_gpu_memory_frac" : 0.65,
-        "enable_plate_recognition" : True,
-        "plate_service_timeout" : 0.5,
-        "gpu_to_use" : "0"
-    }
+    CarConfig.cropper["gpu_memory_frac"] = 0.2
+    CarConfig.cropper["gpu_to_use"] = "0"
+    CarConfig.classifier["gpu_memory_frac"] = 0.2
+    CarConfig.crcl["classifier_gpu_memory_frac"] = 0.65
+    CarConfig.crcl["enable_plate_recognition"] = True
+    CarConfig.crcl["plate_service_timeout"] = 0.5
+    CarConfig.crcl["gpu_to_use"] = "0"
 
 
 class LatteFaceConfig(FaceConfig):
-
-    server = {
-        "gpu_to_use" : "0"
-    }
+    FaceConfig.server["gpu_to_use"] = "0"
